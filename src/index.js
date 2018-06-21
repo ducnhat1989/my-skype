c03ff6d833094a43c4c98dc44d86519fa8de89d1
Duc Nhat Truong <t-nhat@ruby-dev.jp>
2018-06-21 12:09:35 +0700

    Install redux and create store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store';
+
+const render = () => {
+  fancyLog();
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+render();
+store.subscribe(render);
 registerServiceWorker();
+
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉👇", "background: purple; color: #FFF");
+  console.log(store.getState());
+};
